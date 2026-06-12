3f4d051ca9d9159fdd51b3d076d907c25f39e04d
Bowen <411435461@qq.com>
2026-06-12 11:44:12 +0800

    changed reset pose

--- arm_real_client/reset_arm_client.py
+++ arm_real_client/reset_arm_client.py
@@ -4,9 +4,7 @@ robot = RobotClient()
 
 points = [
     {"b": 0, "s": 0, "e": 0, "w": 0, "h": 0},
-    {"b": 30, "s": 0, "e": 0, "w": 0, "h": 0},
-    {"b": 45, "s": 0, "e": 0, "w": 0, "h": 0},
-    {"b": 0, "s": 0, "e": 0, "w": 0, "h": 0},
+    {"b": 0, "s": 0, "e": 0, "w": -90, "h": 0},
 ]
 
 print(robot.trajectory(points, dt=1.0, traj_id="test", spd=10, acc=10))
